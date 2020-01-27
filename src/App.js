153f564cbdfb0c8ae81e33087309bd2f330bbf12
tomlebl <tl12@st-andrews.ac.uk>
2020-01-27 10:46:15 +0000

    minor refactoring

--- src/App.js
+++ src/App.js
@@ -3,52 +3,50 @@ import Cards from './components/Cards/Cards'
 import './App.css'
 
 class App extends Component {
-   state = {
-      cards: [
-         {
-            id: 't-mcap',
-            title: 'Total market cap',
-            value: `${200} B $`
-         },
-         {
-            id: 't-vol',
-            title: '24h volume',
-            value: `${53} B $`
-         },
-         {
-            id: 'btc-dom',
-            title: 'Bitcoin dominance',
-            value: `${65} %`
-         },
-         {
-            id: 'avg-change',
-            title: 'Average change',
-            value: `${1.35} %`
-         },
-         {
-            id: 'vol-change',
-            title: 'Volume change',
-            value: `${5.1} B $%`
-         }
-      ]
-   }
+	state = {
+		cards: [
+			{
+				id: 't-mcap',
+				title: 'Total market cap',
+				value: `${200} B $`
+			},
+			{
+				id: 't-vol',
+				title: '24h volume',
+				value: `${53} B $`
+			},
+			{
+				id: 'btc-dom',
+				title: 'Bitcoin dominance',
+				value: `${65} %`
+			},
+			{
+				id: 'avg-change',
+				title: 'Average change',
+				value: `${1.35} %`
+			},
+			{
+				id: 'vol-change',
+				title: 'Volume change',
+				value: `${5.1} B $%`
+			}
+		]
+	}
 
-   deleteCardHandler = (cardIndex) => {
-      const cards = [...this.state.cards]
-      cards.splice(cardIndex, 1)
-      this.setState({ cards: cards })
-   }
+	deleteCardHandler = cardIndex => {
+		const cards = [...this.state.cards]
+		cards.splice(cardIndex, 1)
+		this.setState({ cards: cards })
+	}
 
-   render() {
-      return (
-         <div className='App'>
-                       <h1>Coin Scanner</h1>
-            <div className='container-cards'>
-               <Cards cardsData={this.state.cards} clicked={this.deleteCardHandler} />
-            </div>
-         </div>
-      )
-   }
+	render() {
+		return (
+			<div className='App'>
+				<h1>Coin Scanner</h1>
+				<Cards cardsData={this.state.cards} clicked={this.deleteCardHandler} />
+			</div>
+		)
+	}
 }
 
 export default App
--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -1,17 +1,18 @@
 import React from 'react'
 import Card from './Card/Card'
+import classes from './Cards.module.css'
 
-const cards = props =>
-   props.cardsData.map((card, index) => {
-      return (
-         <Card
-            key={index}
-            id={card.id}
-            title={card.title}
-            value={card.value}
-            click={() => props.clicked(index)}
-         />
-      )
-   })
+const cards = props => {
+	const cardArr = props.cardsData.map((card, index) => (
+		<Card
+			key={index}
+			id={card.id}
+			title={card.title}
+			value={card.value}
+			click={() => props.clicked(index)}
+		/>
+	))
+	return <div className={classes.Cards}>{cardArr}</div>
+}
 
 export default cards
